cd314ab3811726bf9bb3e3a05a48b41e55a7d053
pontocomdev <emersoncarvalho@outlook.com>
2018-08-24 01:36:58 -0300

    Atualização 24-08-2018

--- OnePage/js/main.js
+++ OnePage/js/main.js
@@ -6,4 +6,14 @@
 		time: 1000,
 		delay: 10
 	});
+
+	var $doc = $('html,body');
+	$('.scroll-page').click(function(){
+		$doc.animate({
+			scrollTop: $($.attr(this,'href')).offset().top
+		},500);
+		return false;
+	});
+
+	
 })(jQuery);
